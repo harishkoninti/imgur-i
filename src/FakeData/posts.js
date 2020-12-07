e819b5f9d1b861af1f2609c655f015d6ebaaf2f9
harish <koninti9@gmail.com>
2020-12-07 22:07:45 +0530

    src

---
+++ src/FakeData/posts.js
@@ -0,0 +1,109 @@
+export default [
+	{
+		id:0,
+		title:"image from imgur",
+		img:"https:\\\\i.imgur.com\\i2g1itt.jpeg",
+		author:'Thor',
+		upVotes:140,
+		comments:[
+			{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'captain'
+			},
+            {
+                id:1,
+                comment:'this is the best post i have seen!! on the best page in world',
+                author:'Thor'
+            }
+		],
+		views:0
+	},
+    {
+    	id:1,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/2ewtXxQ.jpg",
+    	author:'Thor99',
+    	upVotes:24,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'captain'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:2,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/5jR4ntd.jpeg",
+    	author:'captain',
+    	upVotes:444,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:3,
+    	title:"image from imgur",
+    	img:"https:\\\\i.imgur.com/HSo8e0g.jpeg",
+    	author:'Thor',
+    	upVotes:4,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'captain'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:4,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/d8HfE1K.jpg",
+    	author:'captain',
+    	upVotes:124,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'captain'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:5,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/aB3NUoc.jpeg",
+    	author:'captain',
+    	upVotes:124,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:6,
+    	title:"image from imgur",
+    	img:"https:\\\\i.imgur.com/eB0wQz8.jpg",
+    	author:'Thor',
+    	upVotes:43,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:7,
+    	title:"its time to speak more!",
+    	img:"https:\\\\i.imgur.com/Itx3dR0.jpg",
+    	author:'Thor',
+    	upVotes:94,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'captain'
+			}
+    	],
+		views:0
+    }
+];
